9745bd07380b7d97e39feaa8daccca7ed9e7748e
Eduardo Henrique <eduardo.hor@outlook.com>
2023-12-13 12:33:02 -0300

    :tada: feat: Middleware Admin and SuperAdmin

--- routes/admin.php
+++ routes/admin.php
@@ -7,17 +7,19 @@ use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\OrderController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'admin'], function () {
-  Route::get('/painel', [DashboardController::class, 'index'])->name('dashboard.index');
+Route::middleware(['auth', 'admin'])->group(function () {
+  Route::prefix('admin')->group(function () {
+    Route::get('/painel', [DashboardController::class, 'index'])->name('dashboard.index');
 
-  Route::get('/categorias', [CategoryController::class, 'index'])->name('categories.index');
-  Route::get('/categorias/cadastro', [CategoryController::class, 'create'])->name('categories.create');
+    Route::get('/categorias', [CategoryController::class, 'index'])->name('categories.index');
+    Route::get('/categorias/cadastro', [CategoryController::class, 'create'])->name('categories.create');
 
-  Route::get('/produtos/cadastro', [ProductController::class, 'create'])->name('products.create');
-  Route::get('/produtos', [ProductController::class, 'index'])->name('products.index');
+    Route::get('/produtos/cadastro', [ProductController::class, 'create'])->name('products.create');
+    Route::get('/produtos', [ProductController::class, 'index'])->name('products.index');
 
-  Route::get('/pedidos', [OrderController::class, 'index'])->name('orders.index');
-  Route::get('/pedidos/detalhes', [OrderController::class, 'show'])->name('orders.show');
+    Route::get('/pedidos', [OrderController::class, 'index'])->name('orders.index');
+    Route::get('/pedidos/detalhes', [OrderController::class, 'show'])->name('orders.show');
 
-  Route::get('/clientes', [CustomerController::class, 'index'])->name('customers.index');
+    Route::get('/clientes', [CustomerController::class, 'index'])->name('customers.index');
+  });
 });
